685b636d57f296979821c44ef350bde485a8e47e
Kapil Sharma <kapil.sharma@matellio.com>
2019-02-13 13:35:14 +0530

    changed list style in dev branch

--- index.php
+++ index.php
@@ -22,7 +22,14 @@
     echo "<div style='clear: both; width: 100%; font-size: 14px; font-family: Helvetica; color: #30121d; background: #bcbf77; padding: 20px; text-align: center;'>Current branch: <span style='color:#fff; font-weight: bold; text-transform: uppercase;'>" . $branchname . "</span></div>"; //show it on the page
     ?>
 
-    <ul style="background-color:green;">
+    <style>
+    li {
+        background-color: yellow;
+        list-style-type: decimal;
+    }
+    </style>
+
+    <ul>
         <li>Changes from dev branch</li>
         <li>Changes from s_111 branch - <?= date('Y-m-d H:i:s') ?></li>
         <li>Changes from dev branch - <?= date('Y-m-d H:i:s') ?></li>
