fbec9c8672b1b86a495d43a8c7feaea7b2250b8d
Kapil Sharma <kapil.sharma@matellio.com>
2019-02-13 13:28:10 +0530

    added current git branch name to the page

--- index.php
+++ index.php
@@ -10,6 +10,18 @@
 
 <body>
 
+    <?php
+    $stringfromfile = file('.git/HEAD', FILE_USE_INCLUDE_PATH);
+
+    $firstLine = $stringfromfile[0]; //get the string from the array
+
+    $explodedstring = explode("/", $firstLine, 3); //seperate out by the "/" in the string
+
+    $branchname = $explodedstring[2]; //get the one that is always the branch name
+
+    echo "<div style='clear: both; width: 100%; font-size: 14px; font-family: Helvetica; color: #30121d; background: #bcbf77; padding: 20px; text-align: center;'>Current branch: <span style='color:#fff; font-weight: bold; text-transform: uppercase;'>" . $branchname . "</span></div>"; //show it on the page
+    ?>
+
     <ul style="background-color:green;">
         <li>Changes from dev branch</li>
         <li>Changes from s_111 branch - <?= date('Y-m-d H:i:s') ?></li>
